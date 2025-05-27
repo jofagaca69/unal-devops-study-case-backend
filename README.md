c52a908cd78dcc5367ed37a5ba668cef497c8559
yarenas <ysasarenas@gmail.com>
2025-05-26 22:19:08 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Unal Devops Study Case Backend
+
+### Grupo E
+
+- Yosman Arenas
+- Jose Garcia
+- Andres Rojas
+- Sebastian Rios
---
+++ app/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080; // Use process.env.PORT for containerized apps
+
+app.get("/", (req, res) => {
+  res.send("Hello from Healthcare Node.js Backend on GKE!");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.listen(port, () => {
+  console.log(`Node.js backend listening on port ${port}`);
+});
