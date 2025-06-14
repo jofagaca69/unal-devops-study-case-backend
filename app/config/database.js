3ad72ed941d21873e200fb8c2bb02bb49431e4a6
jofagaca69 <jfgarciac@unal.edu.co>
2025-06-13 21:56:13 -0500

    Cambiar la configuración del host de la base de datos a 'unal-database' en lugar de 'localhost'.

--- app/config/database.js
+++ app/config/database.js
@@ -7,7 +7,7 @@ const { Pool } = pg;
 
 const pool = new Pool({
   user: process.env.DB_USER || 'unal_user',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'unal-database',
   database: process.env.DB_NAME || 'unal_db',
   password: process.env.DB_PASSWORD || 'unal_password',
   port: process.env.DB_PORT || 5432,
